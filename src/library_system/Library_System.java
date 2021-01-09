207fcc1119a3ce5351a0f80caff88bb6fe6bee02
GabgabLasconia12 <53455082+GabgabLasconia12@users.noreply.github.com>
2021-01-09 07:50:21 -0800

    Update Library_System.java

--- src/library_system/Library_System.java
+++ src/library_system/Library_System.java
@@ -23,6 +23,7 @@ public class Library_System extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+       stage.setTitle("Library System");
         stage.setResizable(false);
         stage.setHeight(600);
         stage.setWidth(900);
